12782ae2b944ec1dfc5e73c6968266ec54a70b70
Zhairenhao <75477230+ReenHaoZ@users.noreply.github.com>
2023-10-09 20:26:56 +0800

    Hello World!

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * @author zhairenhao
+ * @date 2023/10/9 20:26
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
